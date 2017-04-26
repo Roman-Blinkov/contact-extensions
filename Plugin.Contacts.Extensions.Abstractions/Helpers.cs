9d32507df6bc92f33cf6982c3aea8119f3fcb22e
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-04-26 21:53:39 +0300

    Add Helper class to use in platform specific projects

---
+++ Plugin.Contacts.Extensions.Abstractions/Helpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Contacts.Extensions.Abstractions
+{
+    public static class Helpers
+    {
+        public static bool IsBadFormat(this string argument)
+        {
+            return string.IsNullOrEmpty(argument) || string.IsNullOrWhiteSpace(argument);
+        }
+    }
+}
