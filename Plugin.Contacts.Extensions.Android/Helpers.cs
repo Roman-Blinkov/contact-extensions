1f9d1078452823d39109e286f1328cc3f6ee0c5b
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-04-26 21:55:02 +0300

    Add Android helper class for starting new activities from intent

---
+++ Plugin.Contacts.Extensions.Android/Helpers.cs
@@ -0,0 +1,25 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.Widget;
+
+namespace Plugin.Contacts.Extensions.Android
+{
+    public static class Helpers
+    {
+        public static void StartActivity(this Intent intent)
+        {
+            try
+            {
+                intent.SetFlags(ActivityFlags.ClearTop);
+                intent.SetFlags(ActivityFlags.NewTask);
+
+                Application.Context.StartActivity(intent);
+            }
+            catch (ActivityNotFoundException ex)
+            {
+                // no app found
+            }
+        }
+    }
+}
