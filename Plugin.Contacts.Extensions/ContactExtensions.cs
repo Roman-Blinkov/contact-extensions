94631bae2e9429f51e9462a50d475b9a5e1b39ab
laurentiustamate94 <laurentiustamate94@yahoo.com>
2017-04-26 21:53:16 +0300

    Add plugin main entry point class

---
+++ Plugin.Contacts.Extensions/ContactExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Plugin.Contacts.Extensions.Abstractions.Interfaces;
+
+namespace Plugin.Contacts.Extensions
+{
+    public sealed class ContactExtensions
+    {
+        private static Lazy<ContactExtensions> contactExtensions
+            = new Lazy<ContactExtensions>(() => new ContactExtensions(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+        public readonly ISmsService SmsService = null;
+        public readonly IEmailService EmailService = null;
+        public readonly IPhoneService PhoneService = null;
+
+        private ContactExtensions()
+        {
+#if !PORTABLE
+            this.SmsService = new SmsService();
+            this.EmailService = new EmailService();
+            this.PhoneService = new PhoneService();
+#endif
+        }
+
+        public static ContactExtensions Current
+        {
+            get
+            {
+                var service = contactExtensions.Value;
+
+                if (service == null)
+                {
+                    throw new NotImplementedException();
+                }
+
+                return service;
+            }
+        }
+    }
+}
